9ef0d80da53ca0ce899e31ee7346511d4dbcfc04
Jonathan C <jsea@uw.edu>
2023-10-27 14:38:47 -0700

    Refactor code to separate Python code out of game.html into the final-game-code.js

---
+++ final-game-code.js
@@ -0,0 +1,15 @@
+cfa_final_game_code = `
+########################### DO NOT EDIT ABOVE THIS LINE ###########################
+
+################################ PYTHON CODE BELOW ################################
+
+# STUDENTS:
+#  - Type your own game code below
+#  - Do not change any code outside of the designated area 
+
+
+
+################################ PYTHON CODE ABOVE ################################
+
+########################### DO NOT EDIT BELOW THIS LINE ###########################
+`
